17e1e074e8bdd1d99d989beda7c365e956da69fa
Rishabh Singh <56874272+thisisrishabh22@users.noreply.github.com>
2022-09-13 12:47:31 +0530

    feat: update practical 5 agglomerative clustering

--- hirerarchial clustering/first.py
+++ hirerarchial clustering/first.py
@@ -1,13 +1,33 @@
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.datasets import make_blobs
 
-x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
-y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+DataSet, y = make_blobs(
+   n_samples=10, n_features=2,
+   centers=3, cluster_std=0.5,
+   shuffle=True, random_state=0
+)
 
-data = list(zip(x, y))
 
-linkage_data = linkage(data, method='ward', metric='euclidean')
+linkage_data = linkage(DataSet, metric='euclidean')
+
+# Scatter Plotting the the agglomerative cluster
+plt.scatter(
+   linkage_data[:, 0], linkage_data[:, 1],
+   c='white', marker='o',
+   edgecolor='black', s=50
+)
+plt.title("Agglomerative Clustering Scattering")
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+plt.show()
+
 dendrogram(linkage_data)
+
+# Plotting Dendogram of agglomerative clustering
+plt.title("Agglomerative Clustering")
+plt.xlabel("x axis")
+plt.ylabel("y axis")
 
 plt.show() 
